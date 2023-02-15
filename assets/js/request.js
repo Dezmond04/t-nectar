7ece3fe6635f890608a6fc68b3ddac77f6c7b577
Alex-Program <sasha-radchenko.2000@mail.ru>
2023-02-15 23:31:34 +0800

    Fix

--- assets/js/request.js
+++ assets/js/request.js
@@ -1,8 +1,11 @@
 const elements = {
-    forms: document.querySelectorAll(".request_form")
+    forms: document.querySelectorAll(".request_form"),
+    telInputs: document.querySelectorAll("input[type='tel']")
 };
 
-elements.forms.forEach(form => {
+const itis = [];
+
+elements.forms.forEach((form, i) => {
     const btn = form.querySelector(".send");
 
     btn.addEventListener("click", function () {
@@ -11,6 +14,7 @@ elements.forms.forEach(form => {
         this.disabled = true;
 
         const formData = new FormData(form);
+        formData.set("phone", itis[i].getNumber());
         fetch("/link.php", {
             method: "POST",
             body: formData
@@ -19,5 +23,25 @@ elements.forms.forEach(form => {
             .then(link => {
                 location.href = link;
             });
+    });
+});
+
+elements.telInputs.forEach((input, i) => {
+    const iti = window.intlTelInput(input, {
+        preferredCountries: ["ae", "ru"],
+        separateDialCode: true,
+        utilsScript: "/assets/js/utils.js",
+        nationalMode: false,
+        formatOnDisplay: true
+    });
+    itis.push(iti);
+
+    input.addEventListener("keyup", function () {
+        if (typeof intlTelInputUtils !== 'undefined') {
+            const currentText = iti.getNumber(intlTelInputUtils.numberFormat.E164);
+            if (typeof currentText === 'string') {
+                iti.setNumber(currentText);
+            }
+        }
     });
 });
